10cf31deb94b86b0aafa37904ebbfbfd34b2904f
Yukitaka Maeda <yumaeda@gmail.com>
2019-04-28 15:35:50 +0900

    Add check if the constants are already defined or not

--- config.php
+++ config.php
@@ -1,9 +1,19 @@
 <?php
-// Constants
-define('TARGET_DATE', '2019-10-01 00:00:00');
-define('TIME_ZONE', 'Asia/Tokyo');
-define('CURRENT_TAX_RATE', 0.08);
-define('NEW_TAX_RATE', 0.1);
+if (!defined('TARGET_DATE')) {
+    define('TARGET_DATE', '2019-10-01 00:00:00');
+}
+
+if (!defined('TIME_ZONE')) {
+    define('TIME_ZONE', 'Asia/Tokyo');
+}
+
+if (!defined('CURRENT_TAX_RATE')) {
+    define('CURRENT_TAX_RATE', 0.08);
+}
+
+if (!defined('NEW_TAX_RATE')) {
+    define('NEW_TAX_RATE', 0.1);
+}
 
 return [
     'tax' => [
