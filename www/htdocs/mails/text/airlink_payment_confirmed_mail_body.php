722fb5b558cbda4460cdbbc1884e2648a39e42a4
Yukitaka Maeda <yumaeda@gmail.com>
2021-04-12 10:21:43 +0900

    Add new lines in for better readability

--- www/htdocs/mails/text/airlink_payment_confirmed_mail_body.php
+++ www/htdocs/mails/text/airlink_payment_confirmed_mail_body.php
@@ -9,10 +9,13 @@ $paidOn = date('Y年m月d日 H時i分');
 $title        = '決済金額のお知らせ';
 $customerName = "$holderName 様";
 $body         = "下記の金額を決済頂きましたのでご連絡申し上げます。
+
 [ 支払い方法 ]
 クレジットカード
+
 [ お支払い日時 ]
 $paidOn
+
 [ お支払金額 ]
 $amount 円
 ";
